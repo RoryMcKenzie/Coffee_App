3bd150ea91a4a955516dfe4bb3e72e5620e9e764
RoryMcKenzie <rorymckenzie@mailbox.org>
2021-01-04 19:29:06 +0000

    Added functionality to Brew Timer button

--- app/src/main/java/com/example/coffee_app/MainActivity.java
+++ app/src/main/java/com/example/coffee_app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.coffee_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton brew_timer = findViewById(R.id.brew_timer_button);
+        brew_timer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activityBrewTimer = new Intent(MainActivity.this, ActivityBrewTimer.class);
+                startActivity(activityBrewTimer);
+            }
+        });
     }
 }
