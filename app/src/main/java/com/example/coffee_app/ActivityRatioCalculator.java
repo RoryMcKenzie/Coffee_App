2bed5f3b71c91646c8d43b01c8549c51558391e7
RoryMcKenzie <rorymckenzie@mailbox.org>
2021-01-08 19:21:51 +0000

    Added ratio selector for calculator

--- app/src/main/java/com/example/coffee_app/ActivityRatioCalculator.java
+++ app/src/main/java/com/example/coffee_app/ActivityRatioCalculator.java
@@ -2,10 +2,18 @@ package com.example.coffee_app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class ActivityRatioCalculator extends Activity {
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityRatioCalculator extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ratio_calculator);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Ratio Calculator");
+        EditText ratioNumber = findViewById(R.id.ratioNumber);
+        ratioNumber.setText("16");
     }
 }
